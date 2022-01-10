e73bd29fd584ea28c0d4e411ebd6ad1657242f9a
Romnako <76477868+Romnako@users.noreply.github.com>
2022-01-10 19:59:23 +0300

    HW Lesson_6

--- Lesson_6/log/server_log_config.py
+++ Lesson_6/log/server_log_config.py
@@ -1,14 +1,15 @@
+import logging
 import logging.handlers
 import os.path
 
 logger = logging.getLogger('chat.server')
 
-formatter = logging.Formatter("%(asctime)s - %(levelname)-8s - %(module)-8s - %(message)s")
+formatter = logging.Formatter("%(asctime)s - %(levelname)-8s - %(module)-8s - %(message)s ")
 
 storage_name = 'log-storage'
 if not os.path.exists(storage_name):
     os.mkdir(storage_name)
-filename = os.path.join(storage_name, 'chat.server.log')
+filename = os.path.join(storage_name,'chat.server.log')
 
 fh = logging.handlers.TimedRotatingFileHandler(filename, encoding='utf-8', when='D', interval=1, backupCount=7)
 fh.setLevel(logging.DEBUG)
@@ -22,4 +23,4 @@ if __name__ == '__main__':
     console.setLevel(logging.DEBUG)
     console.setFormatter(formatter)
     logger.addHandler(console)
-    logger.info('Test launch of logging')
+    logger.info('Test run of logging')
